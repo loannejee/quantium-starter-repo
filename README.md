db0d1fec3e8a7759713cc918650bb8cac8ef3877
Joanne <jl7105@nyu.edu>
2022-07-31 16:36:16 -0400

    task 4

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 This repo contains everything you need to get started on the program! Good luck!
 
 
-###### Task 3: 
+###### Task 3: Create a Dash Application
 Issues:
 <br/>
 RequestsDependencyWarning: urllib3 (1.26.11) or chardet (3.0.4) doesn't match a supported version!
@@ -15,3 +15,15 @@ pip3 install --upgrade requests
 
 ###### Goal: Were sales higher before or after the Pink Morsel price increase on the 15th of January, 2021?
 ![pink_morsel_visualizer_soul_foods](https://user-images.githubusercontent.com/95735163/181996238-35c5c308-efce-4d39-a8fe-6f579e78f480.PNG)
+
+###### Task 4: Improve Your Dash Application
+Dash Layout
+https://dash.plotly.com/layout
+<br/>
+<br/>
+Basic Dash Callbacks
+https://dash.plotly.com/basic-callbacks
+<br/>
+<br/>
+Python Figure Reference: layout
+https://plotly.com/python/reference/layout/
--- app.py
+++ app.py
@@ -1,10 +1,18 @@
+# Run this app with `python app.py` and
+# visit http://127.0.0.1:8050/ in your web browser.
 import pandas
-from dash import Dash, html, dcc
+from dash import Dash, html, dcc, Input, Output
 
 from plotly.express import line
 
 # the path to the formatted data file
 DATA_PATH = "./combined_and_cleaned_model.csv"
+# 
+COLORS = {
+    "primary": "#FFEFF2",
+    "secondary": "#ECF9FF",
+    "font": "#522A61"
+}
 
 # load in data
 data = pandas.read_csv(DATA_PATH)
@@ -14,24 +22,94 @@ data = data.sort_values(by="date")
 dash_app = Dash(__name__)
 
 # create the visualization
-line_chart = line(data, x="date", y="sales", title="Pink Morsel Sales")
+def generate_figure(chart_data):
+    line_chart = line(chart_data, x="date", y="sales", title="Pink Morsel Sales")
+    line_chart.update_layout(
+        plot_bgcolor=COLORS["secondary"],
+        paper_bgcolor=COLORS["primary"],
+        font_color=COLORS["font"],
+        title_font_size=22,
+    )
+    return line_chart
+
 visualization = dcc.Graph(
     id="visualization",
-    figure=line_chart
+    figure=generate_figure(data) # data from line 19
 )
 
 # create the header
 header = html.H1(
     "Pink Morsel Visualizer",
-    id="header"
+    id="header",
+    style={
+        "color": COLORS["font"],
+        "font-family": "'Arial', Sans-serif",
+        "padding-top": "20px"
+    }
 )
 
-# define the app layout
+'''
+Create radio button to filter sales by region
+
+The first argument are the options: ["north", "east", "south", "west", "all"]. 
+The second argument is the value property by default at: "north".
+This value of region_picker can change based on your which button you select.
+
+'''
+region_picker = dcc.RadioItems(
+    ["north", "east", "south", "west", "all"],
+    "north",
+    id="region_picker",
+    inline=True
+)
+region_picker_wrapper = html.Div(
+    [
+        region_picker
+    ],
+    style={
+        "font-size": "150%",
+        "padding-bottom": '20px'
+    }
+)
+
+
+# Callback =======================================================================================================
+'''
+Define the region picker callback
+
+Whenever an input property changes (value of region_picker), the function that the callback decorator wraps will get called automatically. 
+Dash provides this callback function with the new value of the input property as its argument, and Dash updates 
+the property of the output component with whatever was returned by the function: update_graph(region).
+
+'''
+@dash_app.callback(
+    Output(visualization, "figure"),
+    Input(region_picker, "value")
+)
+# The new value of the input property (region) gets passed into the function
+def update_graph(region):
+    # filter the dataset
+    if region == "all":
+        trimmed_data = data
+    else:
+        trimmed_data = data[data["region"] == region]
+
+    # generate a new line chart with the filtered data
+    figure = generate_figure(trimmed_data)
+    return figure
+
+
+# define the app layout ==========================================================================================
 dash_app.layout = html.Div(
     [
         header,
-        visualization
-    ]
+        visualization,
+        region_picker_wrapper
+    ],
+    style={
+        "textAlign": "center",
+        "background-color": COLORS["primary"],
+    }
 )
 
 # this is only true if the module is executed as the program entrypoint
